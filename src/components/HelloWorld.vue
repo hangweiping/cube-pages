aa74ecb347a33c564ae0ae087ad15464b12f40cc
hangweiping <hangweiping@126.com>
2019-04-20 16:10:55 +0800

    init4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,72 +1,22 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,
-      <br>check out the
-      <a
-        href="https://cli.vuejs.org"
-        target="_blank"
-        rel="noopener"
-      >vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-        >babel</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-        >eslint</a>
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a>
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a>
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a>
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a>
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-        >vue-devtools</a>
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a>
-      </li>
-    </ul>
+    <a :[names]="url">aaaa</a>
+    <img :[sss]="url" alt />
+
+    <div id="demo">
+      姓：<input type="text" placeholder="firstName" v-model="firstName" />
+      <br />
+      名：<input type="text" placeholder="lastName" v-model="lastName" /><br />
+      姓名1(单向):
+      <input type="text" placeholder="FullName1" v-model="fullName1" />
+      <br />
+      姓名2(双向):
+      <input type="text" placeholder="FullName3" v-model="fullName3" />
+      <br />
+      {{ fullName3 }}
+      <button @click="changeName()">点击改变</button>
+    </div>
   </div>
 </template>
 
@@ -75,6 +25,44 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      names: 'href',
+      sss: 'src',
+      url: '/img/test.png',
+
+      firstName: 'A',
+      lastName: 'B',
+      fullName2: 'A B',
+    };
+  },
+  computed: {// 计算属性相当于data里的属性
+    // 什么时候执行：初始化显示/ 相关的data属性发生变化
+    fullName1() { // 计算属性中的get方法，方法的返回值就是属性值
+      return `${this.firstName} ${this.lastName}`;
+    },
+
+    fullName3: {
+      get() { // 回调函数 当需要读取当前属性值是执行，根据相关数据计算并返回当前属性的值
+        return `${this.firstName} ${this.lastName}`;
+      },
+      set(val) { // 监视当前属性值的变化，当属性值发生变化时执行，更新相关的属性数据
+        // val就是fullName3的最新属性值
+        console.log(val);
+        const names = val.split(' ');
+        console.log(names);
+        [this.firstName, this.lastName] = names;
+      },
+    },
+  },
+  updated() {
+    console.log('updated');
+  },
+  methods: {
+    changeName() {
+      this.fullName3 = 'John Doe';
+    },
   },
 };
 </script>
